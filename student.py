7b39717b0b3d47934dd823f234349858d9705a4d
Niaz Khan <momandniazkhan@gmail.com>
2024-08-14 17:09:36 +0500

    define attributes

--- student.py
+++ student.py
@@ -3,13 +3,16 @@ Create a class `Student` with attributes for name, grades (a list), and methods 
 average, and determine the letter grade.
 Implement a function to handle user input for adding grades and calculating the results.
 """
+
+
 class Student:
     def __init__(self):
-        pass
+        self.st_name = None
+        self.st_grades = []
+        self.grade = None
 
     def add_grades(self):
         pass
 
     def calculate_average(self):
         pass
-
