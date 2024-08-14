0510bc3aa18907084e9e44ea0cb015456b226458
Niaz Khan <momandniazkhan@gmail.com>
2024-08-14 17:08:10 +0500

    initial commit

---
+++ student.py
@@ -0,0 +1,15 @@
+"""
+Create a class `Student` with attributes for name, grades (a list), and methods to add grades, calculate
+average, and determine the letter grade.
+Implement a function to handle user input for adding grades and calculating the results.
+"""
+class Student:
+    def __init__(self):
+        pass
+
+    def add_grades(self):
+        pass
+
+    def calculate_average(self):
+        pass
+
